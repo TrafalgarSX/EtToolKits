e0403f675de5919d6dcc4d89a3956f6022c6acc7
trafalgar <guoyawen98@gmail.com>
2025-05-04 22:31:34 +0800

    Update application metadata in main.cpp for consistency and clarity

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,20 @@
 #include <DelegateUI/delapp.h>
 #include "random_test/RandomTest.h"
 
+const QString applicationVersion = "0.0.1";
+
 int main(int argc, char* argv[])
 {
     QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
     QQuickWindow::setDefaultAlphaBuffer(true);
 
     QGuiApplication app(argc, argv);
-    app.setOrganizationName("MenPenS");
-    app.setApplicationName("DelegateUI");
-    app.setApplicationDisplayName("DelegateUI Gallery");
-    app.setApplicationVersion(DelApp::libVersion());
+    app.setOrganizationName("Trafalgar");
+    app.setApplicationName("EtToolKits");
+    app.setApplicationDisplayName("EtToolKits");
+    app.setApplicationVersion(applicationVersion);
 
     QQmlApplicationEngine engine;
-
     DelApp::initialize(&engine);
 
     RandomTest randomTest;
